6463aac680d566e2ee149370bd9f7792340379f5
jkszymczak <242548@edu.p.lodz.pl>
2023-03-15 15:57:38 +0100

    poprawione sieczne

--- sieczne.py
+++ sieczne.py
@@ -1,23 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
+#stare wzorki
+#st=(( func(x[0])*x[1] )-( func(x[1])*x[0] ))/(func(x[0])-func(x[1]))
+#st=x[0]-( (( func(x[0])*x[1] )-( func(x[1])*x[0] ))/(func(x[0])-func(x[1])) )
 
 def sieczne(przedzialA, przedzialB, liczbaIteracji, dokladnosc, func):
     #przypisanie końców przedziału do x
     x=[przedzialA,przedzialB]
     pr=przedzialA
-    # wzór do liczenia stycznych
-    for i in range(0,liczbaIteracji-1):
-        st=(( func(x[0])*x[1] )-( func(x[1])*x[0] ))/(func(x[0])-func(x[1]))
-        #st=x[0]-( (( func(x[0])*x[1] )-( func(x[1])*x[0] ))/(func(x[0])-func(x[1])) )
-        #st=przedzialA
-        if (abs(st-pr)<dokladnosc):
-            print("miejsce zerowe: ",st)
-            pom=np.linspace(przedzialA,przedzialB,100)
-            plt.plot(pom,  func(pom),'r')
-            plt.plot(pom, 0*pom,'r')
-            plt.show()
-            return
-        pr=st
-        x[0],x[1]=st,x[0]
-        if(x[0]>x[1]):
-            x[0],x[1]=x[1],x[0]
+    st=przedzialB
+    #warunek -> liczba iteracji
+    if liczbaIteracji != -1:
+        for i in range(0,liczbaIteracji-1):
+            st=x[1]-((func(x[1])*(x[1]-x[0]))/(func(x[1])-func(x[0])))
+            st=round(st,2)
+            pr=st
+            x[0],x[1]=st,x[0]
+            if(x[0]>x[1]):
+                x[0],x[1]=x[1],x[0]
+
+    else:
+        #Warunek -> dokładność
+        while(True):
+            st=x[1]-((func(x[1])*(x[1]-x[0]))/(func(x[1])-func(x[0])))
+            if(abs(st-pr)<dokladnosc):
+                break
+            pr=st
+            x[0],x[1]=st,x[0]
+            if(x[0]>x[1]):
+                x[0],x[1]=x[1],x[0]
+        
+
+    print("miejsce zerowe: ",st)
+    pom=np.linspace(przedzialA,przedzialB,100)
+    plt.plot(pom,  func(pom),'r')
+    plt.plot(pom, 0*pom,'r')
+    plt.show()
