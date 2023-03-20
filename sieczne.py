54e171d7ca58b455a84cba42878dece965ff2d1e
jkszymczak <242548@edu.p.lodz.pl>
2023-03-20 15:07:47 +0100

    chyba faktycznie działające sieczne

--- sieczne.py
+++ sieczne.py
@@ -59,23 +59,41 @@ def sieczne(przedzialA, przedzialB, liczbaIteracji, dokladnosc, func):
     x=[przedzialA,przedzialB]
     pr=przedzialA
     st=przedzialB
+    dok=0
+    iteracje=0
+#     #===============================================#
+#     # warunek -> liczba iteracji                    #
+#     # st -> wyliczanie stycznych                    #
+#     # pr -> poprzednia wartość st                   #
+#     # dok -> dokładność wyliczenia miejsca zerowego #
+#     # poPrzecinku -> ile liczb po przecinku wypisać #
+#     #===============================================#
     
     #warunek -> liczba iteracji
     if liczbaIteracji != -1:
+        iteracje=liczbaIteracji
         for i in range(0,liczbaIteracji-1):
             st=x[1]-((func(x[1])*(x[1]-x[0]))/(func(x[1])-func(x[0])))
-            st=round(st,2)
+            #st=round(st,2)
+            dok=abs(st-x[1])
             x[0],x[1]=st,x[0]
             
     else:
             #Warunek -> dokładność
         while(True):
-               st=x[1]-((func(x[1])*(x[1]-x[0]))/(func(x[1])-func(x[0])))
-               if(abs(st-x[1])<dokladnosc):
-                   break
-               x[0],x[1]=x[1],st
+            iteracje+=1
+            st=x[1]-((func(x[1])*(x[1]-x[0]))/(func(x[1])-func(x[0])))
+            dok=abs(st-x[1])
+            if(dok<dokladnosc):
+                  break
+            x[0],x[1]=x[1],st
 
-    print("miejsce zerowe: ",st)
+
+    print("===METODA==SIECZNYCH===")
+    print("miejsce zerowe: ",np.format_float_positional(st))
+    print("Dokladnosc: ", np.format_float_positional(dok))
+    print("Liczba Iteracji: ", iteracje)
+    print("=======================")
     pom=np.linspace(przedzialA,przedzialB,100)
     plt.plot(pom,  func(pom),'r')
     plt.plot(pom, 0*pom,'r')
