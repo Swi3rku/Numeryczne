8bfeb5a1a7445e822b68de41bd620edcfc29ccc2
jkszymczak <242548@edu.p.lodz.pl>
2023-03-31 23:51:48 +0200

    Podział repo na zadania + mały resarch w zad 2

---
+++ Zad1/horner.py
@@ -0,0 +1,5 @@
+def horner(x, a, l):
+    result = a[0]
+    for i in range(1, l):
+        result = result * x + a[i]
+    return result
---
+++ Zad2/main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib as mp
+
+## z tego co dokumentacja numpy mówi to matrix jest wzasadzie porzucony i zalecają korzystać z array
+## wymiary array można oddczytać za pomocą .shape, które zwraca tuple, długość można oddczytać za pomocą len()
