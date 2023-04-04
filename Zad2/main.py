add09f6e4c3705914ca21af597a67a328379c3ee
Jakub Świerczyński <jakswiercz@gmail.com>
2023-04-04 18:00:19 +0200

    Zadanie 2 - dodanie rozpoznawania czy ukad jest sprzeczny czy nieoznaczony, drobne poprawki

--- Zad2/main.py
+++ Zad2/main.py
@@ -1,18 +1,23 @@
 import numpy as np
-import sys
 
 
-def wczytajPlik(nazwaPliku):
+def wczytajPlik(nazwaPliku, lista):
     plik = open(nazwaPliku, "r", encoding="utf8")
     i = 0
     for wiersz in plik:
         wierszLista = wiersz.split(' ')
         j = 0
         for liczba in wierszLista:
-            a[i][j] = float(liczba)
+            lista[i][j] = float(liczba)
             j += 1
         i += 1
-    # return a
+    return lista
+
+
+def zaokr(lista):
+    eps=1e-8
+    mask = np.isclose(lista,0,atol=eps)
+    lista[mask] = 0.0
 
 
 def eliminacjaGaussa(lista, liczbaNiewiadomych):
@@ -20,22 +25,24 @@ def eliminacjaGaussa(lista, liczbaNiewiadomych):
         # Find row with largest pivot element and swap with current row
         max_row = i + np.argmax(abs(lista[i:, i]))
         lista[[i, max_row]] = lista[[max_row, i]]
-        if (round(lista[i][i], 4) == 0):
-            print("znaleziono 0 na przekatnej")
-            sys.exit()
         # Eliminate lower triangular part of the matrix
         for j in range(i + 1, n):
             c = lista[j, i] / lista[i, i]
             lista[j, i:] = lista[j, i:] - c * lista[i, i:]
+    zaokr(lista)
+    print(lista)
+    if(lista[len(lista)-1][len(lista)-1]==lista[len(lista)-1][len(lista)]):
+        print("Układ nieoznaczony")
+        return -1
+    if(lista[len(lista)-1][len(lista)-1]==0 and lista[len(lista)-1][len(lista)]!=0):
+        print("Układ sprzeczny")
+        return -1
+    return 0
 
 
 def podstawianieWsteczne(macierz, liczbaNiewiadomych):
-    # Making numpy array of n size and initializing
-    # to zero for storing solution vector
     wynik = np.zeros(liczbaNiewiadomych)
-    # # Back Substitution
     wynik[liczbaNiewiadomych - 1] = macierz[liczbaNiewiadomych - 1][liczbaNiewiadomych] / macierz[liczbaNiewiadomych - 1][liczbaNiewiadomych - 1]
-
     for i in range(liczbaNiewiadomych - 2, -1, -1):
         wynik[i] = macierz[i][liczbaNiewiadomych]
         for j in range(i + 1, liczbaNiewiadomych):
@@ -44,15 +51,17 @@ def podstawianieWsteczne(macierz, liczbaNiewiadomych):
     return wynik
 
 
+
 n = 0
-while (n != -1):
+while (True):
+    print("Zakoncz program: -1")
     n = int(input('Podaj liczbe niewiadomych: '))
-    fileName = input('Podaj nazwe pliku: ')
-    # Making numpy array of n x n+1 size and initializing
-    # to zero for storing augmented matrix
-    a = np.zeros((n, n + 1))
-    wczytajPlik(fileName)
-    print('\n', a)
-    eliminacjaGaussa(a, n)
-    print('\n', a)
-    print(podstawianieWsteczne(a, n))
+    if(n==-1):
+        break
+    fileName = input('\nPodaj nazwe pliku: ')
+    macierz = np.zeros((n, n + 1))
+    macierz = wczytajPlik(fileName,macierz)
+    print("\nWczytano macierz:")
+    print(macierz, '\n')
+    if(eliminacjaGaussa(macierz, n) == 0):
+        print(podstawianieWsteczne(macierz, n))
