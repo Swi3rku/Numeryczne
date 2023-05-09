08ba7bae9a825425105fdf7de227e7ead36510b3
jkszymczak <242548@edu.p.lodz.pl>
2023-05-10 01:06:54 +0200

    zad 4 end

--- Zad4/main.py
+++ Zad4/main.py
@@ -1,18 +1,44 @@
 import math
+from horner import horner
+import numpy as np
 
-def f(x):
-    return x**2
+#==============================#
+#                              #
+#  Code By string(74*83)(1+')  #
+#                              #
+#==============================#
 
-def g(x):
+def hermiteFunc(x):
     return pow(math.e,-x**2)
 
-def zlozenie (x):
-    return (x**2) * math.exp(-x**2)
+def zlozenie (function):
+    def func(x):
+        return function(x)*hermiteFunc(x)
+    return func
+
+
+def f(x):
+    return horner(x,[1,0,1,0,60],5)
+    # return x**2+x**4+60
+def g(x):
+    return (x * np.sin(x) ** 2)
+def funMod(x):
+    return abs(abs(x-4)-5)
+def notFunLine(x):
+    return 3*x-5
 
 def simpson(f, a, b):
     wynik = ((b-a)/6)*(f(a)+4*f((a+b)/2)+f(b))
     return wynik
 
+def h(x):
+    return np.sin(x) + np.sin(3*x) + np.sin(5*x)
+
+#==============================#
+#                              #
+#  Code By string(74*83)(1+')  #
+#                              #
+#==============================#
 def podzielNaPrzedzialy(a,b,liczbaPrzedzialow):
     przedzialy = []
     przesuniecie = (b-a)/liczbaPrzedzialow
@@ -34,10 +60,37 @@ def newtonCotes(f, a, b, dokladnosc):
         przedzialy = podzielNaPrzedzialy(a,b,liczbaPrzedzialow)
         for i in range(liczbaPrzedzialow):
             wynik += simpson(f,przedzialy[i][0],przedzialy[i][1])
-            # print(wynik)
         liczbaPrzedzialow = liczbaPrzedzialow * 2
     return wynik
 
+def newtonCotesInf(f, step, dokladnosc):
+    wynikDod=0
+    a,b=0,1
+
+    czyPierwszyRazDod = True
+    while(True):
+        obliczoneDod=simpson(f,a,b)
+        wynikDod+=obliczoneDod
+        a,b=b,b+step
+        if(abs(obliczoneDod)<=dokladnosc):
+            break
+
+    obliczoneUj=-10
+    wynikUj=0
+    a,b=-1,0
+    czyPierwszyRazUj=True
+    while(abs(obliczoneUj) > dokladnosc or czyPierwszyRazUj):
+        czyPierwszyRazUj=False
+        obliczoneUj=simpson(f,a,b)
+        wynikUj+=obliczoneUj
+        a,b=a-step,a
+
+    return wynikUj+wynikDod
+#==============================#
+#                              #
+#  Code By string(74*83)(1+')  #
+#                              #
+#==============================#
 def gaussHermite(f, n):
     wynik = 0
     if n == 2:
@@ -52,8 +105,30 @@ def gaussHermite(f, n):
         wynik += x[i] * f(a[i])
     return wynik
 
+#==============================#
+#                              #
+#  Code By string(74*83)(1+')  #
+#                              #
+#==============================#
+zbiorFunc=[f, g, h, funMod, notFunLine]
+functions = [
+    "x^4 + x^2 + 60",
+    "(x * sin(x) ^ 2)",
+    "||x - 4| - 5|",
+    "3*x - 5"
+]
+print("Podaj dokładość dla Newtona-Cotesa: ")
+dokladnosc=float(input())
+print("Podaj wartość kroku dla Newtona-Cotesa: ")
+krok=float(input())
+print("Podaj N dla Gaussa-Hermite'a: ")
+n=int(input())
+for i in range(len(zbiorFunc)-1):
+    print("Wartość całki funkcji ",functions[i]," metodą Newtona-Cotesa dla dokładnosci: ",dokladnosc," i kroku: ",krok,": ",newtonCotesInf(zlozenie(zbiorFunc[i]),krok,dokladnosc))
+    print("Wartość całki funkcji ",functions[i]," metodą Gaussa-Hermite'a dla N=",n,": ",gaussHermite(zbiorFunc[i],n))
 
-
-# print(podzielNaPrzedzialy(0,10,10))
-print(newtonCotes(zlozenie, -1, 1, 0.001))
-print(gaussHermite(f, 4))
+#==============================#
+#                              #
+#  Code By string(74*83)(1+')  #
+#                              #
+#==============================#
